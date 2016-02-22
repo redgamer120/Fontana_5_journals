a6a7ce71a4c0d6663c057765b68160a9927b1eef
D22 <Redgamer120@ninja18.d22.lan>
2016-02-22 13:02:21 -0600

    javascript notes

---
+++ Fontana_5_notes/hello.js
@@ -0,0 +1,15 @@
+alert('Howdy!')
+window.alert('this is a window')
+window.confirm('shall we continue?')
+var r = confirm ("Press a button");
+if (r == true) {
+
+        x= "You pressed OK!";
+} else {
+
+        x= "You pressed Cancel!";
+}
+
+var x = 42;
+document.write(x);
+
